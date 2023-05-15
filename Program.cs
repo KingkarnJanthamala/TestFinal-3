94bfdf26d93b657675e9da4a69c4141045098004
Kingkarn <Kingkarn.jant@mail.kmutt.ac.th>
2023-05-15 15:58:12 +0700

    save1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        string FileInput = Console.ReadLine(); //FileInput คือ ที่อยู่ของไฟล์ข้อมูลภาพนำเข้า
+        string FileConvolutionKernel = Console.ReadLine(); //FileConvolutionKernel คือ ที่อยู่ของไฟล์ข้อมูล Convolution Kernel
+        string FileResult = Console.ReadLine(); //FileResult คือ ที่อยู่ของไฟล์ข้อมูลภาพผลลัพธ์
+        ReadImageDataFromFile(FileInput);
+        ReadImageDataFromFile(FileConvolutionKernel);
+
+
+    }
+    static double[,] ReadImageDataFromFile(string imageDataFilePath) {
+	string[] lines = System.IO.File.ReadAllLines(imageDataFilePath);
+	int imageHeight = lines.Length;
+	int imageWidth = lines[0].Split(',').Length;
+	double[,] imageDataArray = new double[imageHeight, imageWidth];
+ 
+	for(int i=0; i<imageHeight; i++)
+	{
+    	string[] items = lines[i].Split(',');
+    	for(int j=0; j<imageWidth; j++)
+    	{
+        	imageDataArray[i, j] = double.Parse(items[j]);
+    	}
+	}
+	return imageDataArray;
+}
+}
